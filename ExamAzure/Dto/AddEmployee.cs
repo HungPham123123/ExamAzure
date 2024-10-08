be112b718ce8a152edb387ec318f7ee81b59588f
HungPham123123 <129242472+HungPham123123@users.noreply.github.com>
2024-10-08 10:11:58 +0700

    push

---
+++ ExamAzure/Dto/AddEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamAzure.Dto
+{
+    public class AddEmployee
+    {
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime EmployeeDOB { get; set; }
+
+        [Required]
+        public string EmployeeDepartment { get; set; }
+    }
+}
